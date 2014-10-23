aff75285a91fb0ec09eafb53f5c4158f29098022
AndrewSB <asbreckenridge@me.com>
2014-10-23 04:06:05 -0500

    previous project state. Can branch now

---
+++ donut-selfie/DoughnutViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DoughnutViewController.swift
+//  donut-selfie
+//
+//  Created by Andrew Breckenridge on 10/23/14.
+//  Copyright (c) 2014 sprinkles. All rights reserved.
+//
+
+import UIKit
+
+class DoughnutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func addNutButtonWasHit(sender: AnyObject) {
+    }
+
+}
